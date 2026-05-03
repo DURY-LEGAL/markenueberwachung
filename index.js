8090f14abcf5da7bc916ef3aa3060a3e9e0a9398
Slarti @ DURY LEGAL <slarti@dury.de>
2026-05-03 22:42:57 +0200

    Add index.js

---
+++ index.js
@@ -0,0 +1,3 @@
+// markenueberwachung - Markenueberwachung - Automatische Ueberwachung von Markenrechtsverletzungen. Powered by DURY LEGAL.
+// Coming soon. Visit https://www.dury.de for more information.
+module.exports = {};
